bb63818fc04831065f9b0b60d0d3a976ac7fd4a7
Yuexi He <z5422454@ad.unsw.edu.au>
2024-07-30 22:33:59 +1000

    empty files in MLS

---
+++ messageapp/src/main/java/com/bitcoinminers/messageapp/MLS/TreeNode.java
@@ -0,0 +1,17 @@
+package com.bitcoinminers.messageapp.MLS;
+
+
+import java.security.MessageDigest;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class TreeNode {
+    // public key 
+    // secrey key
+    // unmerged leaves 
+    // info about parent 
+}
