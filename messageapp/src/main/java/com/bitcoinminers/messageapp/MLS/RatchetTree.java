f9b3586b910f6b0d761fe91312ca427daa3f6026
Yuexi He <z5422454@ad.unsw.edu.au>
2024-07-30 22:28:03 +1000

    empty files in MLS

---
+++ messageapp/src/main/java/com/bitcoinminers/messageapp/MLS/RatchetTree.java
@@ -0,0 +1,85 @@
+package com.bitcoinminers.messageapp.MLS;
+
+import java.security.MessageDigest;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class RatchetTree {
+    private byte[] salt = {1};
+
+    private MessageDigest sendDigest = null;
+    private MessageDigest receiveDigest = null;
+
+    private SecretKeyFactory sendRatchet = null;
+    private SecretKeyFactory receiveRatchet = null;
+    
+    /*
+     * A bit scuffed but split the secret into two halves to initialise the hash functions
+     */
+    public RatchetTree(byte[] secret, boolean isSender) {
+        try {
+            sendDigest = MessageDigest.getInstance("SHA-256");
+            receiveDigest = MessageDigest.getInstance("SHA-256");
+            sendRatchet = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            receiveRatchet = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            
+            int length = secret.length;
+            byte[] b1 = new byte[length / 2];
+            byte[] b2 = new byte[length / 2];
+            for (int i = 0; i < length/2; i++) {
+                b1[i] = secret[i];
+            }
+            for (int i = 0; i < length/2; i++) {
+                b2[i] = secret[i + length/2];
+            }
+            
+            if (isSender) {
+                sendDigest.update(b1);
+                receiveDigest.update(b2);
+            } else {
+                sendDigest.update(b2);
+                receiveDigest.update(b1);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    /*
+     * Get next key for sending
+     */
+    public SecretKey mergeKey() {
+        try {
+            char[] prev = new String(sendDigest.digest()).toCharArray();
+            KeySpec specs = new PBEKeySpec(prev, salt, 1024, 256);
+            byte[] s = sendRatchet.generateSecret(specs).getEncoded();
+            sendDigest.update(s);
+            SecretKey key = new SecretKeySpec(s, "AES");
+            return key;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    /*
+     * Get next key for receiving
+     */
+    public SecretKey nextReceive() {
+        try {
+            char[] prev = new String(receiveDigest.digest()).toCharArray();
+            KeySpec specs = new PBEKeySpec(prev, salt, 1024, 256);
+            byte[] s = receiveRatchet.generateSecret(specs).getEncoded();
+            receiveDigest.update(s);
+            SecretKey key = new SecretKeySpec(s, "AES");
+            return key;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+}
