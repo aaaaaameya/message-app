7ef2c8bd1d972710aaa3fb30ef1ac2826a409657
Yuexi He <z5422454@ad.unsw.edu.au>
2024-08-06 12:40:38 +1000

    eh

---
+++ messageapp/src/main/java/com/bitcoinminers/messageapp/MLS/Commit.java
@@ -0,0 +1,5 @@
+package com.bitcoinminers.messageapp.MLS;
+
+public class Commit {
+    
+}
--- messageapp/src/main/java/com/bitcoinminers/messageapp/MLS/RatchetTree.java
+++ messageapp/src/main/java/com/bitcoinminers/messageapp/MLS/RatchetTree.java
@@ -30,7 +30,7 @@ public class RatchetTree {
         }
     }
     
-        private void resize() {
+    private void resize() {
     
         }
 
